3f0d4e70a8ce82e3208e095ac6819fa1ff035230
Idris <idrisaboladeo@gmail.com>
2019-11-10 01:35:32 +0100

    adds Menu components

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Homepage from './page/Homepage';
+import Header from './component/Header/index';
 
 
 function App() {
   return (
     <div className="App">
-     <Homepage />
+     <Header />
+     <Homepage/>
     </div>
   );
 }
--- my-website/src/component/Header/index.jsx
+++ my-website/src/component/Header/index.jsx
@@ -1,13 +1,74 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
 
-import './homepage.scss';
+import './header.scss';
 
-const Header = () => ( 
-        <div>
-            
+
+
+class Header extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      addClose: false,
+      addShow: false
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      addClose: !this.state.addClose,
+      addShow: !this.state.addShow
+    })
+  }
+
+
+  render(){
+
+     let closeClass = ['menu-btn'];
+      let showClass = [];
+
+    if(this.state.addClose){
+      closeClass.push('close')
+    }
+
+    if(this.state.addShow){
+      showClass.push('show')
+    }
+
+    return(
+      <div className='header'> 
+      <div className={`${closeClass.join(' ')}`} onClick={this.toggle}>
+        <div className="btn-line"></div>
+        <div className="btn-line"></div>
+        <div className="btn-line"></div>
+      </div>
+      <nav className={`${showClass.join(' ')} menu`} >
+        <div className="display-pic">
+          <div className="portrait"></div>
         </div>
+        <ul className={`${showClass.join(' ')} menu-nav`}>
+          <li className={`${showClass.join(' ')} nav-item`}>
+            <Link to=''>Home</Link>
+          </li>
+          <li className={`${showClass.join(' ')} nav-item`}>
+            <Link to=''>About Me</Link>
+          </li>
+          <li className={`${showClass.join(' ')} nav-item`}>
+            <Link to=''>My Work</Link>
+          </li>
+          <li className={`${showClass.join(' ')} nav-item`}>
+            <Link to=''>How to reach me</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
     )
+  }
+       
+}
 
 
 export default  Header;
