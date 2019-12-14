20ec7e0f975dece8eb5de9e8b4879d3262c4266a
Idris <idrisaboladeo@gmail.com>
2019-12-14 12:28:11 +0100

    adds gh-pages and some other pages

---
+++ my-website/src/page/Work/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import './work.scss';
+
+class Work extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+
+    }
+  }
+
+    render() {
+        return (
+            <div className='work'>
+                <div className='bg-img'/>
+                <h2>my <span className='sec-text'>work</span></h2>
+                <h3 className='txt'> check out some of my project...</h3>
+            </div>
+        )
+    }
+}
+
+
+export default Work
