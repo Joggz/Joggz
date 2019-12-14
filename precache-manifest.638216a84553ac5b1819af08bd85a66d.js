dd3138d6f2b42311632c1cb746841d1a2d859437
Idris <idrisaboladeo@gmail.com>
2019-12-14 17:02:28 +0100

    Updates

---
+++ precache-manifest.638216a84553ac5b1819af08bd85a66d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16255a5d766cb81ff0f3cd6812776b95",
+    "url": "/My-website/index.html"
+  },
+  {
+    "revision": "597e3b959b7c3db73ca9",
+    "url": "/My-website/static/css/main.f0af10ac.chunk.css"
+  },
+  {
+    "revision": "25d1579a13349354aa26",
+    "url": "/My-website/static/js/2.04968500.chunk.js"
+  },
+  {
+    "revision": "597e3b959b7c3db73ca9",
+    "url": "/My-website/static/js/main.82d31717.chunk.js"
+  },
+  {
+    "revision": "c72a3e3618403852e2ae",
+    "url": "/My-website/static/js/runtime-main.bee0bc9d.js"
+  },
+  {
+    "revision": "ac2bd0c6251d4af8f7854eb9600245a8",
+    "url": "/My-website/static/media/About-page.ac2bd0c6.png"
+  },
+  {
+    "revision": "db30237c267a8406a795d7fd64a8f5bf",
+    "url": "/My-website/static/media/Screenshot .db30237c.png"
+  },
+  {
+    "revision": "5e238205ce94ffb7fbe9b204127478da",
+    "url": "/My-website/static/media/dp2.5e238205.jpeg"
+  },
+  {
+    "revision": "770c52a6449f0fae1f241c95d93e081b",
+    "url": "/My-website/static/media/homepage.770c52a6.png"
+  },
+  {
+    "revision": "a990abf57141c38faf9acd8ad7d508f5",
+    "url": "/My-website/static/media/work.a990abf5.png"
+  }
+]);
